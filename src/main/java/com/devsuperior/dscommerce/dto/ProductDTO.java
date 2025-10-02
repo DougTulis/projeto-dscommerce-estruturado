95ebc32419a7a932e947485022f9015d79b986c7
Douglas <dengratulis@gmail.com>
2025-10-01 21:03:03 -0300

    Adicionado resttrições de algumas rotas administrativas

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dscommerce.dto;
 
 import com.devsuperior.dscommerce.entities.Product;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+
 
 public class ProductDTO {
 
