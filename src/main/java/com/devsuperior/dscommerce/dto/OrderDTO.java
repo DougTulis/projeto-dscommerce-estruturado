3be9f5a9f9fc147add1a290748483e182f724dde
Douglas <dengratulis@gmail.com>
2025-10-02 16:41:58 -0300

    Saving orders

--- src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderDTO.java
@@ -12,7 +12,10 @@ private Instant moment;
 private OrderStatus status;
 private ClientDTO client;
 private PaymentDTO payment;
+
+@NotEmpty (message = "Deve ter pelo menos um item")
 private List<OrderItemDTO > items = new ArrayList <>();
+
 public OrderDTO() {
 }
 public OrderDTO(Long id, Instant moment , OrderStatus status , ClientDTO
--- src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/OrderRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dscommerce.entities.Order;
-import com.devsuperior.dscommerce.entities.User;
 
 
 @Repository
