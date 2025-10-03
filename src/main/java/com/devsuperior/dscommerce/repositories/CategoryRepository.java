fbc48e650343d6af95c322ebc48be298c72630fb
Douglas <dengratulis@gmail.com>
2025-10-02 21:24:29 -0300

    Get categories

---
+++ src/main/java/com/devsuperior/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dscommerce.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dscommerce.entities.Category;
+
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+	List<Category> findAll();
+	
+}
